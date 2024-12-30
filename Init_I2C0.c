b6371d53844c573a0b1f2e89598a14ec2a7019f5
GRTedlie <griffintedlie@gmail.com>
2024-12-29 21:41:03 -0600

    Adding  void Init_I2C0_Loopback(void);         void I2C0_Loopback_test(void); functions

--- Init_I2C0.c
+++ Init_I2C0.c
@@ -25,6 +25,75 @@ void Init_I2C0(void){
     */
 }
 
+
+void Init_I2C0_Loopback(void){
+    RCGCI2C |= 0x01U;                       //Enable I2C0 and provide clock to I2C0
+    while ((RCGCI2C & 0x01U) == 0){}        //Wait until I2C0 port awake
+    //MCR MTPR
+    I2C0MCR= ((I2C0MCR &~ 0x20U) | 0x10U);  //Master Function enabled, slave disabled
+    I2C0MCR |= 0x01U;                       //Enable loopback Test Mode
+    I2C0MTPR = 0x27U;                       //Setting the SCL clock, high-speed mode not enabled
+}
+
+void I2C0_Loopback_test(void){
+    uint8_t sendTestData = 0x24;
+    uint8_t receiveTestData;
+
+    I2C0MDR = sendTestData;                 //load test data byte into I2C0 data register
+    I2C0MCS = 0x03;                         //START, RUN
+    while(I2C0MCS & 0x01);                  //wait for I2C controller not busy      
+
+    I2C0MCS = 0x07;                         //START, RUN, STOP
+    receiveTestData = I2C0MDR;              //read data from data register into receiveTestData
+
+    if (receiveTestData == sendTestData){
+        //I2C loopback test successful
+    }
+
+    if (receiveTestData != sendTestData){
+        //I2C loopback test not successful
+    }
+
+}
+
+
+void I2C0_Write(uint8_t slaveAddr, uint8_t regAddr, uint8_t data){
+    I2C0MSA = (slaveAddr << 1) & 0xFE;      //Slave Address, write mode 
+    I2C0MDR = regAddr;                      //Reg address want to send data to on device
+    I2C0MCS = 0x03;                         //START, RUN
+    while (I2C0MCS & 0x01);                 //wait for I2C controller not busy
+
+    if (I2C0MCS & 0x02){                    //check for errors
+    I2C0MCS = 0x4;                          //stop I2C data transfer  
+    return;                                 //return from write function
+    }
+
+    //Sending data now to regAddr of device at slaveAddr
+    I2C0MDR = data;  
+    I2C0MCS = 0x5;                          //RUN, STOP
+    while (I2C0MCS & 0x01);                 //wait for I2C controller not busy
+}
+
+uint8_t I2C0_Read(uint8_t slaveAddr, uint8_t regAddr){
+    I2C0MSA = (slaveAddr << 1) & 0xFE;      //Slave Address, write mode
+    I2C0MDR = regAddr;                      //reg address want to read from
+    I2C0MCS = 0x03;                         //START, RUN
+    while (I2C0MCS & 0x01);                 //wait for I2C controller not busy
+
+    if (I2C0MCS & 0x02){                    //check for errors
+    I2C0MCS = 0x4;                          //stop I2C data transfer  
+    return;                                 //return from read function
+    }
+
+    //Reading data now from regAddr of device at slaveAddr
+    I2C0MSA = (slaveAddr << 1) | 0x01;      //Slave Address, read mode 
+    I2C0MCS = 0x7;                          //START, RUN, STOP
+    while (I2C0MCS & 0x01);                 //wait for I2C controller not busy
+
+    return  I2C0MDR;                        //return data read
+
+}
+
     /*//I2C Specific below -- I2C0 --- PB2 is I2C0SCL , PB3 is I2C0SDA
 #define RCGCI2C (*((volatile uint32_t* )0x400FE620U))
 
--- Init_I2C0.h
+++ Init_I2C0.h
@@ -2,11 +2,16 @@
 Auther: Griffin Tedlie
 Init I2C0 function declaration
 AND I2C0 functions for......*/
-
+#include <stdint.h>
 
 #ifndef PORT_I2C0_INIT
 #define PORT_I2C0_INIT
 
 void Init_I2C0(void);
+void Init_I2C0_Loopback(void);
+void I2C0_Loopback_test(void);
+void I2C0_Write(uint8_t slaveAddr, uint8_t regAddr, uint8_t data);
+uint8_t I2C0_Read(uint8_t slaveAddr, uint8_t regAddr);
+
 
 #endif
